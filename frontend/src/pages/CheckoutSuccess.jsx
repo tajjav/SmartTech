7c8de21ffbe2280310255f1a19b5c151c7aeebc2
tajjav <taj.jav@hotmail.com>
2024-03-26 02:48:52 -0600

    added styled-components and formatted checkout success page and not found page

--- frontend/src/pages/CheckoutSuccess.jsx
+++ frontend/src/pages/CheckoutSuccess.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import styled from 'styled-components';
 
 const CheckoutSuccess = () => {
   return (
-    <div>
-      <h2>Checkout Success</h2>
-    </div>
+    <Container>
+      <h1>Checkout Successful</h1>
+      <p>Your order might take some time to process.</p>
+      <p>Check your order status at your profile after about 10mins.</p>
+      <p>
+        Incase of any inqueries contact the support at{" "}
+        <strong>support@smarttech.ca</strong>
+      </p>
+    </Container>
   )
 };
 
 export default CheckoutSuccess;
+
+const Container = styled.div`
+  min-height: 80vh;
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    margin-bottom: 0.5rem;
+    color: #029e02;
+  }
+`;
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,12 +1,33 @@
 import React from "react";
+import styled from 'styled-components';
 
 const NotFound = () => {
   return (
-    <div>
+    <Container>
       <h1>404</h1>
       <h2>Page not found</h2>
-    </div>
+      <p>
+        Incase of any inqueries contact the support at{" "}
+        <strong>support@smarttech.ca</strong>
+      </p>
+    </Container>
   )
 };
 
 export default NotFound;
+
+const Container = styled.div`
+  min-height: 80vh;
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    margin-bottom: 0.5rem;
+    color: #029e02;
+  }
+`;
